3b5aad45a141b020627319abbef4bd2037a0f28f
Sketch Developer <Developer@Sketchs-MacBook-Air-2.local>
2017-11-13 15:05:20 +0530

    Initial Commit

---
+++ Hey Boy Hey Girl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hey Boy Hey Girl
+//
+//  Created by Sketch Developer on 13/11/17.
+//  Copyright © 2017 Sketch Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hey Boy Hey Girl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hey Boy Hey Girl
+//
+//  Created by Sketch Developer on 13/11/17.
+//  Copyright © 2017 Sketch Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
